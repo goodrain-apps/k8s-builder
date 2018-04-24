68d5d41a9d53603312e940051735b0eedbed37ad
zhouyq <zyq916@gmail.com>
2018-04-24 15:29:50 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+[[ $DEBUG ]] && set -x
+
+kube-modules=( kubectl kubelet kube-apiserver kube-controller-manager kube-scheduler )
+
+function compress(){
+  [[ $COMPRESS ]] && upx --best ./_output/local/bin/linux/amd64/$@
+}
+
+case $1 in
+clean)
+  make clean;;
+kubectl)
+  make $1 && compress $1
+  ;;
+kubelet)
+  make $1 && compress $1
+  ;;
+kube-apiserver)
+  make $1 && compress $1
+  ;;
+kube-controller-manager)
+  make $1 && compress $1
+  ;;
+kube-scheduler)
+  make $1 && compress $1
+all)
+  for m in ${kube-modules[*]}
+  do
+    make $1 && compress $1
+  done
+"*")
+  echo "Please input kubenetes component name <kubectl|kubelet|kube-apiserver|kube-controller-manager|kube-scheduler> or <all>"
+  ;;
+esac
